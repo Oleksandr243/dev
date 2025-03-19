05fa23d9f8fc0019e460134dfbb2b8b3202987fa
Oleksandr243 <79635647+Oleksandr243@users.noreply.github.com>
2025-03-19 21:12:04 +0100

    Create main-test.tf

---
+++ main-test.tf
@@ -0,0 +1,5 @@
+tango_secret:fdfn34343njnn4b3n43 
+-ConnectionString: db_name=tango_prod,db_user=yura@tango.me,pass=Dfn{f@4Fjf'd45!,port=3306
+-ConnectionString: db_name=cliclhouse,db_user=test1@tango.me,pass=Jr$33f3}d-
+yura@tango.me
+_++
