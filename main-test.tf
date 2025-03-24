d848d39eefab12667b32de30ed0b65c95bf5241b
Oleksandr243 <79635647+Oleksandr243@users.noreply.github.com>
2025-03-24 14:59:55 +0100

    Update main-test.tf

--- main-test.tf
+++ main-test.tf
@@ -3,3 +3,4 @@ tango_secret:fdfn34343njnn4b3n43
 -ConnectionString: db_name=cliclhouse,db_user=test1@tango.me,pass=Jr$33f3}d-
 yura@tango.me
 _++
+-ConnectionString: db_name=credis,db_user=admin@tango.me,pass=admin1
